c69daa5058790ebe42b10a9e82a82c5aced0381b
boringthegod <pierre.ceberio@ynov.com>
2024-09-20 14:39:41 +0200

    1.2.0

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,14 @@ use indicatif::{ProgressBar, ProgressStyle};
 use num_cpus;
 use rand::seq::SliceRandom;
 use reqwest::header::HeaderMap;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::net::{Ipv4Addr, SocketAddr};
 use std::str::FromStr;
 use std::sync::Arc;
 use std::time::Duration;
+use tokio::io::AsyncWriteExt;
 use tokio::sync::Semaphore;
 use tokio::time::timeout;
 use colored::*;
@@ -19,21 +21,36 @@ use scraper::{Html, Selector};
 #[tokio::main]
 async fn main() {
     let matches = App::new("CloudFade")
-        .version("1.1")
+        .version("1.2")
         .author("boring")
         .about("Unmask real IP address of a domain hidden behind Cloudflare by IPs bruteforcing")
         .arg(
             Arg::with_name("domain")
+                .long("domain")
+                .short("d")
                 .help("Domain to unmask")
-                .required(true)
-                .index(1),
+                .required_unless("targetsfile")
+                .conflicts_with("targetsfile")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("targetsfile")
+                .long("targetsfile")
+                .short("t")
+                .value_name("TARGETS_FILE")
+                .help("File containing list of domains")
+                .takes_value(true)
+                .conflicts_with("domain")
+                .required_unless("domain"),
         )
         .arg(
             Arg::with_name("ipfile")
+                .long("ipfile")
+                .short("i")
                 .help("File containing list of IP addresses")
                 .conflicts_with("iprange")
                 .required_unless("iprange")
-                .index(2),
+                .takes_value(true),
         )
         .arg(
             Arg::with_name("iprange")
@@ -69,9 +86,24 @@ async fn main() {
                 .help("Set the maximum number of concurrent threads")
                 .takes_value(true),
         )
+        .arg(
+            Arg::with_name("output")
+                .long("output")
+                .value_name("OUTPUT_FILE")
+                .help("File to write the results")
+                .takes_value(true),
+        )
         .get_matches();
 
-    let domain = matches.value_of("domain").unwrap();
+    let domains = if let Some(domain) = matches.value_of("domain") {
+        vec![domain.to_string()]
+    } else if let Some(targets_file) = matches.value_of("targetsfile") {
+        read_lines(targets_file)
+    } else {
+        eprintln!("You must specify either a domain with --domain, or a domain file with --targetsfile.");
+        return;
+    };
+
     let ip_file = matches.value_of("ipfile");
     let ip_range = matches.value_of("iprange");
     let ua_file = matches.value_of("useragents");
@@ -79,15 +111,16 @@ async fn main() {
     let timeout_duration = Duration::from_secs(timeout_secs);
     let aggressive = matches.is_present("aggressive");
 
-    let available_threads = num_cpus::get();
+    let num_cpus = num_cpus::get();
+    let default_threads = num_cpus * 10;
 
     let max_threads = matches
         .value_of("threads")
-        .map(|s| s.parse::<usize>().unwrap_or(available_threads))
-        .unwrap_or(available_threads);
+        .map(|s| s.parse::<usize>().unwrap_or(default_threads))
+        .unwrap_or(default_threads);
 
     println!(
-        "Utilisation de : {} threads",
+        "Use of : {} threads",
         format!("{}", max_threads).blue()
     );
 
@@ -102,19 +135,22 @@ async fn main() {
             }
         }
     } else {
-        eprintln!("Vous devez spécifier soit un fichier d'adresses IP, soit une plage d'adresses IP.");
+        eprintln!("You must specify either an IP address file with --ipfile, or an IP address range with --iprange.");
         return;
     };
 
     if ips.is_empty() {
-        eprintln!("Aucune adresse IP trouvée.");
+        eprintln!("No IP address found.");
         return;
     }
 
-    let pb = ProgressBar::new(ips.len() as u64);
+    let total_tasks = domains.len() * ips.len();
+
+    let pb = ProgressBar::new(total_tasks as u64);
     pb.set_style(
         ProgressStyle::default_bar()
-            .template("{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {pos}/{len} ({eta})"),
+            .template("{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {pos}/{len} ({eta})")
+            .progress_chars("#>-"),
     );
 
     let user_agents = if let Some(ua_path) = ua_file {
@@ -128,39 +164,69 @@ async fn main() {
         vec!["Mozilla/5.0 (compatible; CloudFade/1.0; +https://example.com)".to_string()]
     };
 
-    // Extraction du motif unique du domaine cible
-    let target_pattern = match get_target_pattern(domain, timeout_duration).await {
-        Some(pattern) => pattern,
-        None => {
-            eprintln!("Impossible d'extraire un motif unique du domaine cible.");
-            return;
-        }
-    };
+    // Extraction des motifs uniques pour chaque domaine
+    let mut target_patterns = HashMap::new();
+
+    for domain in &domains {
+        let target_pattern = match get_target_pattern(domain, timeout_duration).await {
+            Some(pattern) => pattern,
+            None => {
+                eprintln!(
+                    "Unable to extract a single pattern from the target domain for {}",
+                    domain
+                );
+                continue;
+            }
+        };
+        target_patterns.insert(domain.clone(), target_pattern);
+    }
+
+    if target_patterns.is_empty() {
+        eprintln!("No single motif could be extracted from the target domains.");
+        return;
+    }
+
+    let tasks_list: Vec<(String, String)> = target_patterns
+        .keys()
+        .flat_map(|domain| ips.iter().map(move |ip| (domain.clone(), ip.clone())))
+        .collect();
 
     let semaphore = Arc::new(Semaphore::new(max_threads));
 
     let mut tasks = FuturesUnordered::new();
 
-    for ip in ips {
+    // Préparation du fichier de sortie
+    let output_file = matches.value_of("output");
+    let output_mutex = if let Some(output_path) = output_file {
+        Some(Arc::new(tokio::sync::Mutex::new(
+            tokio::fs::File::create(output_path).await.unwrap(),
+        )))
+    } else {
+        None
+    };
+
+    for (domain, ip) in tasks_list {
+        let domain = domain.clone();
         let ip = ip.trim().to_string();
-        let domain = domain.to_string();
         let user_agents = user_agents.clone();
         let aggressive = aggressive;
         let timeout_duration = timeout_duration;
         let semaphore = semaphore.clone();
         let pb = pb.clone();
-        let target_pattern = target_pattern.clone();
 
-        let permit = semaphore.acquire_owned().await.unwrap();
+        // Récupérer le motif cible pour ce domaine
+        let target_pattern = target_patterns.get(&domain).unwrap().clone();
+
+        let permit = semaphore.clone().acquire_owned().await.unwrap();
 
         tasks.push(tokio::spawn(async move {
             let result = test_ip(
                 ip,
-                domain,
+                domain.clone(),
                 user_agents,
                 aggressive,
                 timeout_duration,
-                target_pattern,
+                target_pattern.clone(),
             )
             .await;
             drop(permit);
@@ -169,27 +235,20 @@ async fn main() {
         }));
     }
 
-    let mut possible_ips = Vec::new();
-
     while let Some(result) = tasks.next().await {
-        if let Ok(Some(ip)) = result {
-            possible_ips.push(ip);
+        if let Ok(Some((domain, ip))) = result {
+            let message = format!("{}: {}", domain.green(), ip.green());
+            pb.println(message.clone());
+
+            if let Some(output_mutex) = &output_mutex {
+                let mut file = output_mutex.lock().await;
+                let line = format!("{}: {}\n", domain, ip);
+                file.write_all(line.as_bytes()).await.unwrap();
+            }
         }
     }
 
-    pb.finish_with_message("Scan terminé");
-
-    if possible_ips.is_empty() {
-        println!(
-            "{}",
-            format!("Aucune IP trouvée liée à {}", domain).red()
-        );
-    } else {
-        println!("{}", "Adresses IP potentielles trouvées :".green());
-        for ip in possible_ips {
-            println!("{}", ip.green());
-        }
-    }
+    pb.finish_with_message("Scan completed");
 }
 
 async fn test_ip(
@@ -199,11 +258,11 @@ async fn test_ip(
     aggressive: bool,
     timeout_duration: Duration,
     target_pattern: String,
-) -> Option<String> {
+) -> Option<(String, String)> {
     let socket_addr: SocketAddr = match format!("{}:80", ip).parse() {
         Ok(addr) => addr,
         Err(_) => {
-            eprintln!("Adresse IP invalide : {}", ip);
+            eprintln!("Invalid IP address : {}", ip);
             return None;
         }
     };
@@ -238,7 +297,7 @@ async fn test_ip(
                 if status == 200 || status == 301 || status == 302 {
                     if let Ok(text) = resp.text().await {
                         if text.contains(&target_pattern) {
-                            return Some(ip);
+                            return Some((domain, ip));
                         }
                     }
                 }
@@ -272,29 +331,39 @@ fn extract_title(html: &str) -> Option<String> {
     let document = Html::parse_document(html);
     let selector = Selector::parse("title").unwrap();
 
-    document.select(&selector).next().map(|element| element.inner_html())
+    document
+        .select(&selector)
+        .next()
+        .map(|element| element.inner_html())
 }
 
 fn read_lines(filename: &str) -> Vec<String> {
-    let file = File::open(filename).expect("Impossible d'ouvrir le fichier");
+    let file = File::open(filename).expect("Unable to open file");
     let reader = BufReader::new(file);
-    reader.lines().filter_map(Result::ok).collect()
+    reader
+        .lines()
+        .filter_map(Result::ok)
+        .map(|line| line.trim().to_string())
+        .filter(|line| !line.is_empty())
+        .collect()
 }
 
 fn parse_ip_range(ip_range: &str) -> Result<Vec<String>, String> {
     let parts: Vec<&str> = ip_range.split('-').collect();
     if parts.len() != 2 {
-        return Err("Le format de la plage IP est invalide. Utilisez le format 'début-fin'.".to_string());
+        return Err("IP range format is invalid. Use 'start-end' format.".to_string());
     }
 
-    let start_ip = Ipv4Addr::from_str(parts[0]).map_err(|_| "Adresse IP de début invalide.".to_string())?;
-    let end_ip = Ipv4Addr::from_str(parts[1]).map_err(|_| "Adresse IP de fin invalide.".to_string())?;
+    let start_ip =
+        Ipv4Addr::from_str(parts[0]).map_err(|_| "Start IP address invalid.".to_string())?;
+    let end_ip =
+        Ipv4Addr::from_str(parts[1]).map_err(|_| "End IP address invalid.".to_string())?;
 
     let start: u32 = start_ip.into();
     let end: u32 = end_ip.into();
 
     if start > end {
-        return Err("L'adresse IP de début est supérieure à l'adresse IP de fin.".to_string());
+        return Err("The starting IP address is greater than the ending IP address.".to_string());
     }
 
     let max_ips = 1_000_000;
@@ -302,7 +371,7 @@ fn parse_ip_range(ip_range: &str) -> Result<Vec<String>, String> {
 
     if total_ips > max_ips {
         return Err(format!(
-            "La plage d'IP est trop grande ({} adresses). Veuillez spécifier une plage plus petite.",
+            "IP range is too large ({} addresses). Please specify a smaller range.",
             total_ips
         ));
     }
